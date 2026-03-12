2fa8c7df0c5dca774482deb65546cf916af951e1
Andrew Robbertz <24920994+Alrobbertz@users.noreply.github.com>
2026-03-12 17:39:30 -0400

    Update Imports for Docs Build

--- swxsoc/util/__init__.py
+++ swxsoc/util/__init__.py
@@ -2,3 +2,10 @@ from swxsoc.util.exceptions import *
 from swxsoc.util.util import *
 from swxsoc.net.attr import *
 from swxsoc.net.client import *
+
+# Limit automodapi documentation to only util-native symbols;
+# net re-exports are kept for backward compatibility but documented under swxsoc.net.
+from swxsoc.util.exceptions import __all__ as _exceptions_all
+from swxsoc.util.util import __all__ as _util_all
+
+__all__ = _exceptions_all + _util_all
